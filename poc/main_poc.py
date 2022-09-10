307e59ce193e1241e1e842e16dd0ec9b684ec3a1
josifgrabocka <33453152+josifgrabocka@users.noreply.github.com>
2022-09-11 01:15:42 +0200

    working version

--- poc/main_poc.py
+++ poc/main_poc.py
@@ -10,15 +10,15 @@ num_evals = len(X)
 print("Num evals in meta-dataset", num_evals)
 
 # initial design indices
-#num_initial_observations = 3
-#initial_design_idxs = random.sample(range(num_evals), num_initial_observations)
+num_initial_observations = 1
+initial_design_idxs = random.sample(range(num_evals), num_initial_observations)
 
-initial_design_idxs = [250, 550, 850]
+#initial_design_idxs = [550]
 
 # create an smbo method
 config = {'is_rank_version': True, 'eta': 0.01, 'optim_iters': 300, 'train_batch_size': 30,
           'acquisition_batch_size': 1000, 'log_iters': 300, 'hidden_layers_units': [16, 16, 16, 16],
-          'use_batch_norm': False, 'use_dropout': False, 'dropout_rate': 0.0, 'beta': 7.0}
+          'use_batch_norm': False, 'use_dropout': False, 'dropout_rate': 0.0, 'beta': 3.0}
 
 nn_bo = NeuralNetworkBOSynth(config=config)
 
